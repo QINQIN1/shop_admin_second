7c269506c093094b380a34d092c333384f6089fa
QINQIN1 <941054391@qq.com>
2019-01-03 16:40:31 +0800

    登录页搭建完成

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,59 +1,83 @@
 <template>
-  <div>登录组件
-  <el-button round>圆角按钮</el-button>
-  <el-button type="primary" round>主要按钮</el-button>
-  <el-button type="success" round>成功按钮</el-button>
-  <el-button type="info" round>信息按钮</el-button>
-  <el-button type="warning" round>警告按钮</el-button>
-  <el-button type="danger" round>危险按钮</el-button>
-
-  <el-table
-      :data="tableData"
-      style="width: 100%">
-      <el-table-column
-        prop="date"
-        label="日期"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="地址">
-      </el-table-column>
-    </el-table>
-  </div>
-
-
+  <el-form
+    :model="loginForm"
+    :rules="rules"
+    ref="loginForm"
+    label-width="100px"
+    class="demo-loginForm"
+    label-position="top"
+  >
+    <el-form-item label="用户名" prop="username">
+      <el-input v-model="loginForm.username"></el-input>
+    </el-form-item>
+    <el-form-item label="密码" prop="password">
+      <el-input v-model="loginForm.password">
+      </el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
+      <el-button @click="resetForm('loginForm')">重置</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 <script>
-  export default {
-      data() {
-        return {
-          tableData: [{
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-          }, {
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄'
-          }, {
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
-          }, {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          }]
-        }
+// 导入axios
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      loginForm: {
+      //  用户名
+      username:'',
+      password:''
+      },
+      rules: {
+        username: [
+          // required 表示必填项
+          // message 表示校验失败时要展示的错误信息
+          // trigger 表示触发表单验证的时机
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 5, max: 12, message: "用户名长度为5到12个字符", trigger: "blur" }
+        ],
+         password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 12, message: "密码长度为6到12个字符", trigger: "blur" }
+        ]
       }
+    }
+  },
+  methods: {
+    // 表单验证的代码逻辑
+    submitForm(formName) {
+
+      // $refs是vue提供的一个对象，用来获取页面中所有带有ref属性的元素
+      // this.$refs[formName]和 this.$refs.loginForm是相同的
+      // console.log(this.$refs);
+      this.$refs[formName].validate(valid => {
+        // console.log(valid)
+       if(!valid){
+        //  验证失败代码中不需要处理
+         return false
+       }
+      //  表单验证成功
+      // 1.获取用户名和密码
+      console.log(this.loginForm)
+      // 2.调用登录接口完成登录
+          axios
+          .post('http://localhost:8888/api/private/v1/login', this.loginForm)
+          .then(res => {
+            console.log(res)
+          })
+      // 3.登陆成功跳转到首页
+      });
+    },
+
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
   }
+};
 </script>
 <style>
-
 </style>
